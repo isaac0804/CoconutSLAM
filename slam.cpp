c0ab3ee1de030a9cc48fcb753766978284ac1c56
Isaac Yong <isaacyong0804@gmail.com>
2021-07-16 00:15:36 +0800

    detect and display

--- slam.cpp
+++ slam.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp> 
 #include <opencv2/features2d/features2d.hpp>
@@ -9,11 +10,14 @@ using namespace cv;
 
 
 void extract(Mat &image) {
-  int MAX_FEATURES = 50000;
+  int MAX_FEATURES = 1000;
   vector<KeyPoint> keyPoints; 
   Mat descriptors; 
   Ptr<ORB> orb = ORB::create(MAX_FEATURES);
   orb->detect(image, keyPoints);
+  for(auto keypoint : keyPoints) {
+    circle(image, keypoint.pt, 4, Scalar(0, 255, 0));
+  }
   cout << "Number of features: " << keyPoints.size() << endl;
 }
 
@@ -39,9 +43,14 @@ int main(int argc, char **argv) {
     if(frame.empty()) break;
 
     // Feature Extraction using ORB
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
     extract(frame);  
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    chrono::duration<double> interval = chrono::duration_cast<chrono::duration<double>>(end - start);
+    cout << "Time used         : " << interval.count() << endl;
       
     // Display
+    resize(frame, frame, Size(frame.cols/2, frame.rows/2));
     imshow("Video", frame);
     char c = (char)waitKey(1000/cap.get(5)); // display according to original fps
     if(c==27) break; // Esc 
