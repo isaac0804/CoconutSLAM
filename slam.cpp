3f6c899c68940f2b63bedbb328564c6e275b891f
Isaac Yong <isaacyong0804@gmail.com>
2021-07-16 09:19:25 +0800

    more even feature detection

--- slam.cpp
+++ slam.cpp
@@ -17,19 +17,31 @@ class Frame {
     Frame(int frame_index, Mat input_image) {
       index = frame_index;
       image = input_image;
-      cout << "Register Frame " << index << endl;
     }
 };
 
-void extractAndMatch(Mat &image) {
-  int MAX_FEATURES = 1000;
+void extractAndMatch(Frame &curr_frame, Frame &prev_frame) {
+  int MAX_FEATURES = 5000;
+  float scale = 0.1; // must be integers..... 
+  int frame_width = curr_frame.image.cols * scale;
+  int frame_height = curr_frame.image.rows * scale;
+
   vector<KeyPoint> keyPoints; 
   Mat descriptors; 
-  Ptr<ORB> orb = ORB::create(MAX_FEATURES);
-  orb->detect(image, keyPoints);
-  for(auto keypoint : keyPoints) {
-    circle(image, keypoint.pt, 4, Scalar(0, 255, 0));
+  Ptr<ORB> orb = ORB::create(MAX_FEATURES*scale*scale);
+
+  for(size_t y = 0; y < 1/scale; y++)
+  for(size_t x = 0; x < 1/scale; x++) {
+    vector<KeyPoint> curr_keyPoints; 
+    Mat curr_descriptors; 
+    Rect roi = Rect(x*frame_width, y*frame_height, frame_width, frame_height);
+    orb->detect(curr_frame.image(roi), curr_keyPoints);
+    for(auto keypoint : curr_keyPoints) {
+      circle(curr_frame.image, keypoint.pt+Point2f(x*frame_width, y*frame_height), 4, Scalar(0, 255, 0));
+    }
+    keyPoints.insert(keyPoints.end(), curr_keyPoints.begin(), curr_keyPoints.end());
   }
+
   cout << "Number of features: " << keyPoints.size() << endl;
 }
 
@@ -54,19 +66,19 @@ int main(int argc, char **argv) {
     Mat frame;
     cap >> frame;
     if(frame.empty()) break;
-    frames.push_back(Frame(cap.get(1)-1, frame));
+    frames.push_back(Frame(cap.get(CAP_PROP_POS_FRAMES)-1, frame));
 
     // Feature Extraction using ORB
     chrono::steady_clock::time_point start = chrono::steady_clock::now();
-    extractAndMatch(frame);  
+    extractAndMatch(frames[cap.get(CAP_PROP_POS_FRAMES)-1], frames[cap.get(CAP_PROP_POS_FRAMES)-2]);  
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
     chrono::duration<double> interval = chrono::duration_cast<chrono::duration<double>>(end - start);
     cout << "Time used         : " << interval.count() << endl;
       
     // Display
-    resize(frames[cap.get(1)-1].image, frame, Size(frame.cols/2, frame.rows/2));
+    resize(frames[cap.get(CAP_PROP_POS_FRAMES)-1].image, frame, Size(frame.cols/2, frame.rows/2));
     imshow("Video", frame);
-    char c = (char)waitKey(1000/cap.get(5)); // display according to original fps
+    char c = (char)waitKey(1000/cap.get(CAP_PROP_FPS)); // display according to original fps
     if(c==27) break; // Esc 
   }
 
