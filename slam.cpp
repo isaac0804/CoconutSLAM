829ba6c2476490b08ed27709f544805c6074aeb2
Isaac Yong <isaacyong0804@gmail.com>
2021-07-16 15:14:33 +0800

    gf2t is better than orb even without cut regions

--- slam.cpp
+++ slam.cpp
@@ -4,6 +4,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp> 
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 
 using namespace std;
 using namespace cv;
@@ -23,56 +25,60 @@ class Frame {
 };
 
 void extractAndMatch(Frame &curr_frame, Frame &prev_frame) {
-  int MAX_FEATURES = 4000;
-  float scale = 0.2; // must be integers..... 
+  int MAX_FEATURES = 3000;
+  float scale = 1.0; // must be integers..... also this affects the time needed for each frame drastically. how can i improve?
   int frame_width = curr_frame.image.cols * scale;
   int frame_height = curr_frame.image.rows * scale;
 
+  Mat gray;
+  cvtColor(curr_frame.image, gray, CV_BGR2GRAY);
+
   vector<KeyPoint> keypoints; 
   Mat descriptors; 
-  Ptr<ORB> orb = ORB::create(MAX_FEATURES);
+  vector<Point2f> corners; 
+  //Ptr<ORB> orb = ORB::create(MAX_FEATURES);
+  //Ptr<ORB> orb = ORB::create(MAX_FEATURES*scale*scale);
 
   // Find the features evenly by doing the grid
-  //for(size_t y = 0; y < 1/scale; y++)
-  //for(size_t x = 0; x < 1/scale; x++) {
-  //  vector<KeyPoint> curr_keyPoints; 
-  //  Rect roi = Rect(x*frame_width, y*frame_height, frame_width, frame_height);
-  //  Mat mask = Mat::zeros(curr_frame.image.size(), CV_8UC1);
-  //  mask(roi).setTo(Scalar::all(255));
-  //  orb->detect(curr_frame.image, curr_keyPoints, mask);
-  //  for(auto curr_keypoint : curr_keyPoints) {
-  //    curr_keypoint.pt = curr_keypoint.pt + Point2f(x*frame_width, y*frame_height);
-  //    keyPoints.push_back(curr_keypoint);
-  //  }
-  //}
+  for(size_t y = 0; y < 1/scale; y++)
+  for(size_t x = 0; x < 1/scale; x++) {
+    vector<Point2f> curr_corners; 
+    Rect roi = Rect(x*frame_width, y*frame_height, frame_width, frame_height);
+    Mat mask = Mat::zeros(curr_frame.image.size(), CV_8UC1);
+    mask(roi).setTo(Scalar::all(255));
+    goodFeaturesToTrack(gray, curr_corners, MAX_FEATURES*scale*scale, 0.1, 8, mask, 3, false, 0.04);
+    corners.insert(corners.end(), curr_corners.begin(), curr_corners.end());
+  }
 
   // Find keypoints and Compute descriptors
-  orb->detectAndCompute(curr_frame.image, noArray(), keypoints, descriptors, false);
-  curr_frame.kps = keypoints;
-  curr_frame.dps = descriptors;
+  // orb->detectAndCompute(curr_frame.image, noArray(), keypoints, descriptors, false);
+  //curr_frame.kps = keypoints;
+  //curr_frame.dps = descriptors;
 
   // Match keypoints based on descriptors
   BFMatcher matcher;
   vector<vector<DMatch>> knn_matches;
-  matcher.knnMatch(descriptors, prev_frame.dps, knn_matches, 2);
+  //matcher.knnMatch(descriptors, prev_frame.dps, knn_matches, 2);
 
   // Filter invalid matches 
-  const double thresh = 0.7;
+  const double thresh = 0.6;
   vector<DMatch> good_matches;
-  for(size_t i = 0; i < knn_matches.size(); i++) {
-    if(knn_matches[i][0].distance < thresh * knn_matches[i][1].distance) {
-        good_matches.push_back(knn_matches[i][0]);
-    }
-  }
+  //for(size_t i = 0; i < knn_matches.size(); i++) {
+  //  if(knn_matches[i][0].distance < thresh * knn_matches[i][1].distance) {
+  //      good_matches.push_back(knn_matches[i][0]);
+  //  }
+ // }
 
   // Draw matches 
-  for(size_t i = 0; i < good_matches.size(); i++) {
-    circle(curr_frame.image, prev_frame.kps[good_matches[i].trainIdx].pt, 4, Scalar(255, 0, 0));
-    line(curr_frame.image, prev_frame.kps[good_matches[i].trainIdx].pt, keypoints[good_matches[i].queryIdx].pt, Scalar(255, 0, 0));
-    circle(curr_frame.image, keypoints[good_matches[i].queryIdx].pt, 4, Scalar(0, 255, 0));
+  for(size_t i = 0; i < corners.size(); i++) {
+    circle(curr_frame.image, corners[i], 4, Scalar(255, 0, 0));
+  //  circle(curr_frame.image, prev_frame.kps[good_matches[i].trainIdx].pt, 4, Scalar(255, 0, 0));
+  //  line(curr_frame.image, prev_frame.kps[good_matches[i].trainIdx].pt, keypoints[good_matches[i].queryIdx].pt, Scalar(255, 0, 0));
+  //  circle(curr_frame.image, keypoints[good_matches[i].queryIdx].pt, 4, Scalar(0, 255, 0));
   }
   
-  cout << "Number of features: " << keypoints.size() << " -> " << good_matches.size() << endl;
+  //cout << "Number of features: " << keypoints.size() << " -> " << good_matches.size() << endl;
+  cout << "Number of features: " << corners.size() << endl;
 }
 
 void initFirstFrame(Frame &first_frame) {
